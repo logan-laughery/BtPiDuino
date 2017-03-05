3262291a9310940e737caf8db8d3ea429a405ed4
Logan Laughery <Logan Laughery>
2017-03-05 11:24:18 -0600

    Added node ignore

--- Src/Node/node-led-control.js
+++ Src/Node/node-led-control.js
@@ -1,15 +1,33 @@
-
-console.log('started script');
+'use strict';
 var BTSP = require('bluetooth-serial-port');
+var inquirer = require('inquirer');
+var BottomBar = inquirer.ui.BottomBar;
 var serial = new BTSP.BluetoothSerialPort();
 
-console.log('loaded deps');
+var devices = [];
+
+var loader = [
+    '/ Searching',
+    '| Searching',
+    '\\ Searching',
+    '- Searching'
+]
+var i = 4;
+var ui = new BottomBar({bottomBar: loader[i % 4]});
+
+setInterval(function () {
+    ui.updateBottomBar(loader[i++ % 4]);
+}, 300);
 
 serial.on('found', function(address, name) {
-    console.log('Device found: address-' + address + ' name-' + name);
+    devices.push({name: address + ' - ' + name, value: {address: address, name: name}});
+    // console.log('Device found: address-' + address + ' name-' + name);
     // you might want to check the found address with the address of your
     // bluetooth enabled Arduino device here.
+});
 
+
+var connect = function (address) {
     serial.findSerialPortChannel(address, function(channel) {
         serial.connect(address, channel, function() {
             console.log('connected');
@@ -18,8 +36,8 @@ serial.on('found', function(address, name) {
 
             process.stdin.on('data', function (data) {
                 serial.write(new Buffer(data, 'utf-8'), function(err, bytesWritten){
-			if (err) console.log(err);
-		});
+			        if (err) console.log(err);
+		        });
             });
 
             serial.on('data', function(buffer) {
@@ -29,6 +47,23 @@ serial.on('found', function(address, name) {
             console.log('cannot connect');
         });
     });
+}
+
+serial.on('finished', function() {
+    ui.updateBottomBar('');
+    ui.close();
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'device',
+            message: 'Which device do you want to connect to?',
+            choices: devices
+        }
+    ]).then(function (answer) {
+        // console.log(JSON.stringify(answer, null, ' '));
+        connect(answer.device.address);
+    }) 
 });
+
 
 serial.inquire();
